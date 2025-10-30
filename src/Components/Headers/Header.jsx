94fc162ef468fe9be16b544e07bbc89d1eb58d1a
Anjaykumarr <92406281+Anjaykumarr@users.noreply.github.com>
2025-10-31 00:27:13 +0530

    Update Header.jsx

--- src/Components/Headers/Header.jsx
+++ src/Components/Headers/Header.jsx
@@ -1,23 +1,18 @@
-import "../../Styles/ComponentStyle/header.css"
-import logo from "../../assets/logo_nrega.jpg"
+import "../../Styles/ComponentStyle/header.css";
+import logo from "../../assets/logo_nrega.jpg";
 import AutoDistrict from "../AutoDistrict/AutoDistrict";
 
-
 function Header() {
-
     return (
-        <>
-            <div className="header">
-                
-                <img src={ logo } alt="Loading..." />
-                
-                <h1>Our Voice, Our Rights</h1>
+        <div className="header">
+            <img src={logo} alt="Logo" />
+            <h1>Our Voice, Our Rights</h1>
 
+            <div className="auto-district-wrapper">
                 <AutoDistrict />
-                
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default Header;
